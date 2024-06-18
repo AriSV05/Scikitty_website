25efd061388303d50f10250bf13bfac0958f8a2a
AriSV05 <80506478+AriSV05@users.noreply.github.com>
2024-06-17 19:31:54 -0600

    nuevo script fake_weights. Se mutean los demas scripts al no ser evaluados directamente

--- run_tb_scripts.bat
+++ run_tb_scripts.bat
@@ -1,5 +1,6 @@
 cd ..
-python -m PaginaIA.demos.CO2
-python -m PaginaIA.demos.diabetes
-python -m PaginaIA.demos.scikit_cancer_full
-python -m PaginaIA.demos.scikit_cancer_very_reduced
+REM python -m PaginaIA.demos.CO2
+REM python -m PaginaIA.demos.diabetes
+REM python -m PaginaIA.demos.scikit_cancer_full
+REM python -m PaginaIA.demos.scikit_cancer_very_reduced
+python -m PaginaIA.demos.fake_weights
